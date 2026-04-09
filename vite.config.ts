e671dc4a50f5a3dbd198ad2a66cd462f3cd35991
Guillajp <jguillaume@u-wellhealth.com>
2026-04-09 12:36:44 -0400

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // <--- ADD THIS LINE RIGHT HERE
+    base: '/steele_truss_company/' // <--- ADD THIS LINE RIGHT HERE
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
