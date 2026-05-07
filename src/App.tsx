ba70b9332be6e45e3ca64995a54dacedfa440abf
Guillajp <jguillaume@u-wellhealth.com>
2026-05-06 21:58:06 -0400

    Change Router from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import RoofTrusses from "./pages/RoofTrusses";
@@ -9,7 +9,7 @@ import FeedbackInternal from "./pages/feedbackInternal";
 
 export default function App() {
   return (
-    <Router basename="/"> 
+    <Router> 
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
